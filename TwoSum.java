b986ec8e16baa85a2d3d891d1b77b49f781ca341
Rajat Pratap <chauhanrajat515@gmail.com>
2025-05-09 22:46:36 +0530

    changes Made

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int targt = 7;
+        int i=0;
+        int j = arr.length-1;
+        while (i<=j) {
+            if(arr[i]+arr[j]>targt){
+                j--;
+            }
+            else if(arr[i]+arr[j]<targt){
+                i++;
+            }
+            else{
+    
+            }
+        }
+        System.out.println("Not found");
+    }
+}
